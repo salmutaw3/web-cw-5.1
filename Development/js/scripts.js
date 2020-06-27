50b40e1e928a2169ed826ca8c736d5beb4362584
salmutaw3 <66717132+salmutaw3@users.noreply.github.com>
2020-06-27 21:51:23 +0300

    خلصت

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,29 +1,64 @@
 // السؤال الأول
 // 1
-
+const name = "صهيب";
 // 2
-
+let age = 15;
 // 3 and 4
+console.log(
+    "انا "
++
 
+ name
+
++
+
+" عمري "
+
+ +
+ age
+);
 // 5 and 6
-
+console.log(5*age);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hobbies = [
+"ركوب الخيل"
+,
+"طبخ"
+];
 // 2
 
-// 3
-
+hobbies.push(age);
+hobbies[3] = 16
+console.log(hobbies.length)
+// 3"
+hobbies.pop();
+console.log(hobbies);
 // 4
-
+let barmej = {
+track: "web",
+bestlanguage: "html",
+};
 // 5
-
+barmej["TAs"] = 
+[
+"نانسي"
+,
+"موضي"
+,
+"رهف"
+,
+"حسين"
+   
+]
+console.log(barmej);
 // 6
-
+console.log(barmej["track"]);
+console.log(barmej.track);
 // 7
-
+console.log(barmej["TAs"][0])
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
